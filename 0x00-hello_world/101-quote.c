f43e27aefef9bfc7d7afefad9ef662c393328497
Amson1 <johnsonameh50@gmail.com>
2023-06-16 18:42:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
  * Description: This  program will  prints  text and sandard error
  * Return: Print out the standard error
  */
-int main(void);
+int main(void)
 { 
 	printf("nd that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-		return(1); 
+	return(1); 
 
 }
 
