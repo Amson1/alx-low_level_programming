0a0dbfcf3dcfbfbf9be83f336713ace3ccebf041
Amson1 <johnsonameh50@gmail.com>
2023-06-16 18:01:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdi.h>
+#include <stdio.h>
 
 int main(void) { 
 
-	puts("\"Programming is like building a multilingual puzzle');
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
