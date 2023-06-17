25cc7f2b03db62f2c58968a7e81bc3bedc4d0a54
Amson1 <johnsonameh50@gmail.com>
2023-06-17 13:05:34 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - this will contain the code executed
- * Return: this will return 0 if successfully, and 1 if not scccessful
- */ 
-int main(void) 
-{ 
+ * main - Entry point
+ *
+ * Description: Prints a line of code using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-} 
-
+}
