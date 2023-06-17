8d26fa3357a5eac3526c84a4a894b7a4db461089
Amson1 <johnsonameh50@gmail.com>
2023-06-17 06:06:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - This is the main point
@@ -6,6 +7,7 @@
  * Return: Print out the standard error
  */
 int main(void)
+
 { 
 	
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
