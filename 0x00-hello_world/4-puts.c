326f4963bbb1c106e5a42384ebdb46f9879cd484
Amson1 <johnsonameh50@gmail.com>
2023-06-16 18:11:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
+
 #include <stdio.h>
 
-int main(void) { 
+/**
+ * main -  A This the name of the function to houses the code
+ * Return: This will return 0 if correct and return 1 if the code fails
+ */
+
+int main(void)
+{ 
 
 	puts("\"Programming is like building a multilingual puzzle");
 
