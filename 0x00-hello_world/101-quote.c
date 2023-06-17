b33edfddde73b089ae133d45635a638307af5b85
Amson1 <johnsonameh50@gmail.com>
2023-06-16 18:37:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: This  program will  prints  text and sandard error
+ * Return: Print out the standard error
+ */
+int main(void);
+{ 
+	printf("nd that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+		return(1); 
+
+}
+
