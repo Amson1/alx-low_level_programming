86aeec66f949efd641f80cec06e8d548e877d667
Amson1 <johnsonameh50@gmail.com>
2023-06-17 13:01:28 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Prints a line of code using puts
+ * Description: Prints a line of code using printf
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
