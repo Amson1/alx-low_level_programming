0ac45079d97c595d199229f285a4ed8335f3f6dd
Amson1 <johnsonameh50@gmail.com>
2023-06-16 17:57:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - this will contain the code executed
+ * Return: this will return 0 if successfully, and 1 if not scccessful
+ */ 
 int main(void) 
 { 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
