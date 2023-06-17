7cd624f1c47d53dc0a1235d4e7a4b28d0b160d59
Amson1 <johnsonameh50@gmail.com>
2023-06-17 06:16:58 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,14 @@
 
 /**
  * main - This is the main point
+ *
  * Description: This  program will  prints  text and sandard error
  * Return: Print out the standard error
+ *
  */
 int main(void)
 
-{ 
-	
+{
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
