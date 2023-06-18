8d587d748d1641b669acc1c18307a080c05f7010
Amson1 <johnsonameh50@gmail.com>
2023-06-18 04:06:01 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -The unmber is the determinant if the output is positive
+ * negative or zero
+ *
+ * Return is 0
+ */
+int main (void)
+{  int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if  (n > 0)
+
+	printf("%d is positive\n", n);
+
+	else if (n == 0)
+
+	printf("%d is zero\n", n);
+
+	else if (n < 0)
+
+	printf("%d is negative\", n);
+
+	return (0);
+
+}
+
+
+
+
