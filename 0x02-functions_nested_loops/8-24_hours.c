299e78569b7141064e98849bba961d773837bdde
Amson1 <johnsonameh50@gmail.com>
2023-06-20 19:43:36 -0700

    updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,17 @@
  */
 void jack_bauer(void)
 {
-	int hrs, mins;
-
-	for (hrs = 0; hrs < 24; hrs++)
-	{
-	for (mins = 0; mins < 60; mins++)
-	{
-	_putchar((hrs / 10) + '0');
-	_putchar((hrs % 10) + '0');
-	_putchar(':');
-	_putchar((mins / 10) + '0');
-	_putchar((mins % 10) + '0');
-	_putchar('\n');
-    }
-  }
+int hrs, mins;
+for (hrs = 0; hrs < 24; hrs++)
+{
+for (mins = 0; mins < 60; mins++)
+{
+_putchar((hrs / 10) + '0');
+_putchar((hrs % 10) + '0');
+_putchar(':');
+_putchar((mins / 10) + '0');
+_putchar((mins % 10) + '0');
+_putchar('\n');
+}
+}
 }
