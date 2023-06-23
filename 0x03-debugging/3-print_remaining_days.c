a1dae0884e2339237e21498aa377bb021d4dc493
Amson1 <johnsonameh50@gmail.com>
2023-06-22 17:32:06 -0700

    updated

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,16 +15,14 @@ if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 if (month > 2 && day >= 60)
 {
 day++;
-}	  
-printf("Day of the year: %d\n", day);
+}printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);        }
 else
 {
 printf("Day of the year: %d\n", day);
@@ -32,3 +30,7 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
+
+
+
+
