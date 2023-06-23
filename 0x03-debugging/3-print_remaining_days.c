a17b517bb09d123f9760cee0ab32286b461d57a7
Amson1 <johnsonameh50@gmail.com>
2023-06-23 10:09:54 -0700

    updated

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,8 @@ if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 if (month > 2 && day >= 60)
 {
 day++;
-}printf("Day of the year: %d\n", day);
+}
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
 else
