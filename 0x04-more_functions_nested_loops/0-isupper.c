4eef3e41bb5a3733fc76421c0f4c873efa9e7fdf
Amson1 <johnsonameh50@gmail.com>
2023-06-23 18:59:28 -0700

    supper c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,11 @@
  *@c: the characters to be check
  * Return: 1 for lowercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90) 
-return (1);
-else
-return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
 }
