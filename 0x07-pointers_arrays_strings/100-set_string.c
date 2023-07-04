58a07eb67b8989aef3a9a67bf98dd03b85132b04
Amson1 <johnsonameh50@gmail.com>
2023-07-04 16:42:46 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,27 +1,13 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_diagsums - The function that sets the value of a pointer to a char.
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
+ * set_string - A function that sets the value of a pointer to a char.
+ * @s: The content which is to be copied
+ * @to: string
+ * Return: void
  */
-void print_diagsums(int *a, int size)
+
+void set_string(char **s, char *to)
 {
-	int sum1, sum2, y;
-
-	sum1 = 0;
-	sum2 = 0;
-
-	for (y = 0; y < size; y++)
-	{
-		sum1 = sum1 + a[y * size + y];
-	}
-	for (y = size - 1; y >= 0; y--)
-	{
-		sum2 += a[y * size + (size - y - 1)];
-	}
-	printf("%d, %d\n", sum1, sum2);
+	*s = to;
 }
-
-
