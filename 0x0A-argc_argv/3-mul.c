79516b331eb532f6c5a958b282f1f08195583f93
Amson1 <johnsonameh50@gmail.com>
2023-07-12 16:51:48 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A function that multiplies two numbers.
+ * @argc: The command line of arguments.
+ * @argv: The array  arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
